742e51d2ad2e0be0102c3322ee004eb56d8ebe31
stickz <stickman002@mail.com>
2017-04-28 12:34:48 -0400

    Fix loading & add changelog

--- lua/VertexsBalance/Client.lua
+++ lua/VertexsBalance/Client.lua
@@ -4,4 +4,4 @@ if gDisableUWEBalance then return end
 --load our own locals
 Script.Load("lua/VertexsBalance/Locale.lua")
 
-GBM_version = 201704272 --year month date versionofdate
+Script.Load("lua/VertexsBalance/Changelog.lua")
---
+++ lua/VertexsBalance/changelog.lua
@@ -0,0 +1,23 @@
+GBM_version = 201704282 --year month date versionofdate
+
+Script.Load("lua/VertexsBalance/GUIModChangelog.lua")
+
+local function showchangelog()
+	MainMenu_Open()
+	local mm = GetGUIMainMenu and GetGUIMainMenu()
+	if mm then
+		local changelog = CreateMenuElement(mm.mainWindow, "GUIModChangelog")
+	end
+end
+
+local function onLoadComplete()
+	local oldversion = Client.GetOptionInteger("balancemod_version", 0)
+	if GBM_version > oldversion then
+		Client.SetOptionInteger("balancemod_version", GBM_version)
+		showchangelog()
+	end
+
+end
+
+Event.Hook("LoadComplete", onLoadComplete)
+Event.Hook("Console_changelog", showchangelog)
